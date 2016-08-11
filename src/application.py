be3c6508d4e85be6652aa45fc7d96566d47258c6
mlegeny <marton.legeny@gmail.com>
2016-08-11 17:43:11 +0100

    Code review

--- src/application.py
+++ src/application.py
@@ -62,3 +62,38 @@ if __name__ == '__main__':
             tag = sys.argv[2]
 
         app(url, tag)
+
+'''
+Code Review
+
+- os and re are not used at all and can be removed (however, they would be useful for unit testing)
+- sys is imported twice (the second one is on line #54)
+- No comment header for the functions (input parameters, output, what does the function do
+- Input arguments could be parsed (argparse) instead of using positional arguments
+- KeyboardInterrupt (Ctrl-C / SIGINT) is not handled
+
+-> get(a)
+   - parameter 'a' do not have a descriptive name
+   - no input sanity check, backtrace will be printed if the URL is not valid
+   - no exception handling
+   - 'response' might be None and calling .text will not be possible
+-> element_count(a)
+   - parameters 'a' & 'b' do not have a descriptive name
+   - useless comment '# element count'
+   - the value of 'output' is not checked
+   - no default parser passed to BeautifulSoup - it prints a warning on Windows 7 64 bit / Python 3
+   - the value of 'soup' is not checked, it might be None
+-> fizz_or_buzz(number)
+   - comment states that "fizz buzz" should be returned if 'number' can be divided by both 3 and 5,
+     but "fizzbuzz" is returned by the code
+   - the logical construct is wrong: the code will return immediately if 'number' can be divided by 3 or by 5,
+     making 'return 'fizzbuzz'' dead code
+-> app_output(*args)
+   - no exception handling (what if the file is write-protected
+   - output is always appended to the existing file, I'm not sure if this is desired by the application
+   - this could be assigned to a variable to avoid code duplication: '{} = {} = {} = {}\n'.format(*args)
+-> app(a,b)
+   - parameters 'a' & 'b' do not have a descriptive name
+   - no input sanity checks
+   - 'FizzBuzz' does not match with the existing naming convention
+'''
